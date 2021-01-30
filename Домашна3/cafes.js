32a0c7042331ae5e69ce1a5a7b15fa2a53d9287f
SaraFazliu <67891916+SaraFazliu@users.noreply.github.com>
2021-01-30 16:20:42 +0100

    Update cafes.js

--- Домашна3/cafes.js
+++ Домашна3/cafes.js
@@ -63,8 +63,8 @@ function showLocation(name){
         var allRows = data.split(/\r?\n|\r/);
         for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
             var rowCells = allRows[singleRow].split(',');
-            console.log(name.innerText);
-            if (rowCells[3] === name.innerText){
+            console.log(name);
+            if (rowCells[3] === name){
                 let to = L.marker({lon: rowCells[2], lat: rowCells[1]}).getLatLng();
                 let from = getLocation();
                 console.log(from + ' ' + to);
@@ -91,7 +91,7 @@ function successFunction(data) {
         var rowCells = allRows[singleRow].split(',');
         if(findGetParameter('where') !== rowCells[4]) continue;
         table += '<tr>';
-        table += '<td onclick="showLocation(this)">';
+        table += '<td onclick="showLocation(this.innerText)">';
         table += rowCells[3] + '</td>';
         table += '<td>' + rowCells[5] + '<br>';
         table += '</td>';
@@ -103,9 +103,10 @@ function successFunction(data) {
     document.getElementById("list").innerHTML=table;
 }
 
-$( function() {
+function  searchbar(){
     var where = findGetParameter('where');
-    var availableTags = [];
+    var arr = [];
+    var inp = document.getElementById('myInput');
     $.ajax({
         url: 'kafe.csv',
         dataType: 'text',
@@ -115,15 +116,60 @@ $( function() {
 
             for (var singleRow = 1; singleRow < allRows.length; singleRow++) {
                 var rowCells = allRows[singleRow].split(',');
-                if(where !== rowCells[4]) continue;
-                availableTags.push(rowCells[3]);
-                console.log(availableTags);
+                if (where !== rowCells[4]) continue;
+                arr.push(rowCells[3]);
             }
 
-            $( "#myInput" ).autocomplete({
-                source: availableTags
+            inp.addEventListener("input", function(e) {
+                var a, b, i, val = this.value;
+                /*close any already open lists of autocompleted values*/
+                closeAllLists();
+                if (!val) { return false;}
+                currentFocus = -1;
+                /*create a DIV element that will contain the items (values):*/
+                a = document.createElement("DIV");
+                a.setAttribute("id", this.id + "autocomplete-list");
+                a.setAttribute("class", "autocomplete-items");
+                /*append the DIV element as a child of the autocomplete container:*/
+                this.parentNode.appendChild(a);
+                /*for each item in the array...*/
+                for (i = 0; i < arr.length; i++) {
+                    /*check if the item starts with the same letters as the text field value:*/
+                    if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                        /*create a DIV element for each matching element:*/
+                        b = document.createElement("DIV");
+                        /*make the matching letters bold:*/
+                        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                        b.innerHTML += arr[i].substr(val.length);
+                        /*insert a input field that will hold the current array item's value:*/
+                        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                        /*execute a function when someone clicks on the item value (DIV element):*/
+                        b.addEventListener("click", function(e) {
+                            /*insert the value for the autocomplete text field:*/
+                            inp.value = this.getElementsByTagName("input")[0].value;
+                            showLocation(this.getElementsByTagName("input")[0].value);
+                            /*close the list of autocompleted values,
+                            (or any other open lists of autocompleted values:*/
+                            closeAllLists();
+                        });
+                        a.appendChild(b);
+                    }
+                }
             });
 
+            function closeAllLists(elmnt) {
+                /*close all autocomplete lists in the document,
+                except the one passed as an argument:*/
+                var x = document.getElementsByClassName("autocomplete-items");
+                for (var i = 0; i < x.length; i++) {
+                    if (elmnt != x[i] && elmnt != inp) {
+                        x[i].parentNode.removeChild(x[i]);
+                    }
+                }
+            }
+            /*execute a function when someone clicks in the document:*/
+            document.addEventListener("click", function (e) {
+                closeAllLists(e.target);
+            });
         });
-
-} );
+}
